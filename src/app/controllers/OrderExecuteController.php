5b89cfbbf8fbc826a03ca216616289e552868a5f
guoshipeng <2249875980@qq.com>
2020-01-08 15:57:47 +0800

    逻辑处理

--- src/app/controllers/OrderExecuteController.php
+++ src/app/controllers/OrderExecuteController.php
@@ -30,7 +30,7 @@ class OrderExecuteController extends BaseController
                     return 0;
                 }
                 $this->order = $result->fetch_assoc();
-                var_dump($this->order);
+
                 //开启事务
                 $this->db->begin_transaction();
                 try {
@@ -55,14 +55,14 @@ class OrderExecuteController extends BaseController
 
     private function _setUserCourse()
     {
-        $result = $this->db->query("select id, course_id from h_order_items where order_id = ".$this->order['id']);
-        if ($rows = $result->fetch_all()) {
-            foreach ($rows as $k => $val) {
-                $type = $this->db->query("select type from h_edu_courses where id = ".$val[1]);
-                $type->fetch_assoc();
+        if ($result = $this->db->query(
+            "select id, course_id from h_order_items where order_id = ".$this->order['id']
+        )) {
+            while ($row = $result->fetch_assoc()) {
+                $type = $this->db->query("select type from h_edu_courses where id = ".$row['course_id'])->fetch_assoc();
                 //先不考虑 训练营的课程
                 $this->db->query(
-                    "insert into h_user_course('type', 'user_id', 'course_id', 'order_id', 'order_item_id', 'created_at', 'updated_at') values({$type}, {$this->order['user_id']}, {$val[1]}, {$this->order['id']}, {$val[0]}, {$this->time}, {$this->time})"
+                    "insert into h_user_course('type', 'user_id', 'course_id', 'order_id', 'order_item_id', 'created_at', 'updated_at') values({$type['type']}, {$this->order['user_id']}, {$row['course_id']}, {$this->order['id']}, {$row['id']}, {$this->time}, {$this->time})"
                 );
             }
 
